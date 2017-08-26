bc93330db387b2cc776bd158aa04d8ce7d0dab5c
Ariven <ariven@Arivens-Mac.local>
2017-08-26 20:12:32 +0530

    lesson01 - Custom TextField

--- window-shopper/MainVC.swift
+++ window-shopper/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  window-shopper
 //
 //  Created by Ariven on 26/08/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ window-shopper/textFieldStyle.swift
@@ -0,0 +1,37 @@
+//
+//  textFieldStyle.swift
+//  window-shopper
+//
+//  Created by Ariven on 26/08/17.
+//  Copyright © 2017 Ariven. All rights reserved.
+//
+
+
+import UIKit
+
+class textFieldStyle: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2467091182)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if placeholder == nil{
+        
+            placeholder = " "
+        }
+        
+        if let p = placeholder{
+        
+            let place = NSAttributedString(string: p, attributes: [NSForegroundColorAttributeName: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+        
+        
+    }
+
+}

b6983d2cf165fa469916a5a912e0f499711a375c
Ariven <ariven@Arivens-Mac.local>
2017-08-26 20:55:59 +0530

    lesson02 - Input Accessory View & IBDesignable

--- window-shopper/textFieldStyle.swift
+++ window-shopper/textFieldStyle.swift
@@ -9,29 +9,37 @@
 
 import UIKit
 
+@IBDesignable
 class textFieldStyle: UITextField {
 
+    override func prepareForInterfaceBuilder() {
+        
+        customizeView()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        customizeView()
+    }
+
+    func customizeView(){
+    
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2467091182)
         layer.cornerRadius = 5.0
         textAlignment = .center
         
         if placeholder == nil{
-        
+            
             placeholder = " "
         }
         
         if let p = placeholder{
-        
+            
             let place = NSAttributedString(string: p, attributes: [NSForegroundColorAttributeName: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
             attributedPlaceholder = place
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
-        
-        
-        
     }
-
+    
 }
